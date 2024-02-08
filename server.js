e8ed8a8246841d38a2ddbd572c70280ef04469d7
galabeketov <beketovgalimjan0@gmail.com>
2024-02-08 11:22:12 +0500

    folder initial commit

--- server.js
+++ server.js
@@ -86,6 +86,4 @@ app.get("/books/:id", (req, res) => {
 });
 
 let SERVER_PORT = 9090;
-app.listen(SERVER_PORT, () =>
-  console.log(`Server is listening on port: ${SERVER_PORT}`)
-);
+app.listen(SERVER_PORT, () => console.log(`Server is listening on port: ${SERVER_PORT}`));
